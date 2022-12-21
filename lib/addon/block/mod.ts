c14aea499f66de908a605a39f0cff979554f4d70
ink0rr <tomflynnbeltsazar@gmail.com>
2022-12-22 03:51:16 +0700

    Delete builders

--- lib/addon/block/mod.ts
+++ lib/addon/block/mod.ts
@@ -1,5 +1,2 @@
-export * from "./builder/BPBlockBuilder.ts";
-export * from "./builder/CreateBlock.ts";
-export * from "./builder/RPBlockBuilder.ts";
 export * from "./BlockBehavior.ts";
 export * from "./BlockResource.ts";
--- lib/addon/entity/mod.ts
+++ lib/addon/entity/mod.ts
@@ -1,5 +1,2 @@
-export * from "./builder/BPEntityBuilder.ts";
-export * from "./builder/CreateEntity.ts";
-export * from "./builder/RPEntityBuilder.ts";
 export * from "./EntityBehavior.ts";
 export * from "./EntityResource.ts";
