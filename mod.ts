4b7b0d514d14936a6136baf42e0f8ff661347620
ink0rr <tomflynnbeltsazar@gmail.com>
2023-09-28 18:31:17 +0700

    Add animation controllers

--- mod.ts
+++ mod.ts
@@ -7,6 +7,7 @@ export * from "./lib/bedrock/common/RawText.ts";
 export * from "./lib/bedrock/common/SpellEffects.ts";
 export * from "./lib/bedrock/common/query.ts";
 
+export * from "./lib/bedrock/animation_controllers/AnimationControllers.ts";
 export * from "./lib/bedrock/animations/Animations.ts";
 export * from "./lib/bedrock/attachable/Attachable.ts";
 export * from "./lib/bedrock/entity/Entity.ts";
