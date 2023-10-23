f1829710a740ebb8a2555ce2e4acb6901ff3fd8f
ink0rr <tomflynnbeltsazar@gmail.com>
2023-10-23 10:07:03 +0700

    Update deps

--- deps.ts
+++ deps.ts
@@ -1,4 +1,3 @@
-export * as JSONC from "https://deno.land/std@0.190.0/jsonc/mod.ts";
-export { dirname } from "https://deno.land/std@0.190.0/path/mod.ts";
-export { join } from "https://deno.land/std@0.190.0/path/posix.ts";
+export * as JSONC from "https://deno.land/std@0.204.0/jsonc/mod.ts";
+export { dirname, join } from "https://deno.land/std@0.204.0/path/posix/mod.ts";
 export { default as startCase } from "https://deno.land/x/lodash@4.17.15-es/startCase.js";
