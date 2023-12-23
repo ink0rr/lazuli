2dce8040e6ddf6fd5f053d5cfd240542143fa14e
ink0rr <tomflynnbeltsazar@gmail.com>
2023-12-23 23:46:44 +0700

    Update deps

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export * as JSONC from "https://deno.land/std@0.204.0/jsonc/mod.ts";
-export { dirname, join } from "https://deno.land/std@0.204.0/path/posix/mod.ts";
+export * as JSONC from "https://deno.land/std@0.210.0/jsonc/mod.ts";
+export { dirname, join } from "https://deno.land/std@0.210.0/path/posix/mod.ts";
--- test/utils/startCase.test.ts
+++ test/utils/startCase.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.204.0/assert/mod.ts";
+import { assertEquals } from "../../dev_deps.ts";
 import { startCase } from "../../lib/utils/startCase.ts";
 
 Deno.test("startCase", () => {
